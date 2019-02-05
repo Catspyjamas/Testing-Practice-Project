6d5a0b11cc7a55efbba7a60eb6bc838ed9c83796
Catspyjamas <judith.urban@peerigon.com>
2019-02-05 18:28:02 +0100

    replaced for with math

--- mondaysInMonth.js
+++ mondaysInMonth.js
@@ -4,42 +4,27 @@ const moment = require("moment");
 
 function mondaysInMonth(month) {
     const first = moment(month, "YYYY-MM", true).startOf("month");
-    // Get the actual number of days in the given month
-    const daysInMonth = first.daysInMonth();
-
-    console.log(daysInMonth);
 
     if (first.isValid() !== true) {
         throw new Error("Please pass a month in this format: 'YYYY-MM'");
     }
-    console.log(first.format());
 
     // Set corresponding Monday
     const mondayThisWeek = first.clone().day(1);
-
-    console.log(mondayThisWeek.format());
-
     let firstMonday;
 
-    // Check if first is a Monday
-    if (mondayThisWeek.isSame(first)) {
-        firstMonday = mondayThisWeek;
-        // Check if first is from Tuesday til Saturday
-    } else if (mondayThisWeek.isBefore(first)) {
-        firstMonday = first.day(1 + 7);
+    if (mondayThisWeek.isBefore(first)) {
+        firstMonday = first.clone().day(1 + 7);
     } else {
-        firstMonday = first.day(1);
+        firstMonday = mondayThisWeek;
     }
 
-    const dateOfFirstMonday = firstMonday.date();
-    // console.log(dateOfFirstMonday);
     // Set Monday counter
     let numberOfMondays = 0;
 
-    // Count up until next month
-    for (let fr = dateOfFirstMonday; fr <= daysInMonth; fr += 7) {
-        numberOfMondays += 1;
-    }
+    numberOfMondays = Math.floor(
+        (first.daysInMonth() - firstMonday.date()) / 7 + 1
+    );
 
     return numberOfMondays;
 }
